c4d70e40bce679b5b05a1a3a732b24444170c375
shq <sihaozi61@qq.com>
2021-08-10 01:37:40 +0800

    fix

--- blenderfunc/render/render.py
+++ blenderfunc/render/render.py
@@ -55,12 +55,12 @@ def render_color(filepath: str = '/tmp/temp.png', samples: int = 32, auto_tile_s
     render_layers_node = node_tree.nodes.new('CompositorNodeRLayers')
     file_output_node = node_tree.nodes.new('CompositorNodeOutputFile')
     file_output_node.base_path = os.path.dirname(filepath)
-    file_output_node.file_slots['Image'].path = ''
+    file_output_node.file_slots['Image'].path = 'image'
     node_tree.links.new(render_layers_node.outputs[0], file_output_node.inputs[0])
     bpy.context.scene.frame_current = 1
     bpy.ops.render.render(use_viewport=True)
 
-    os.rename(os.path.join(output_dir, '0001.png'), filepath)
+    os.rename(os.path.join(output_dir, 'image0001.png'), filepath)
     print('image saved: {}'.format(filepath))
 
 
