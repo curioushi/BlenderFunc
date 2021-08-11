53324bc23b7e8e5cdd995e5a605beb8e0af2ccf6
haoqi shi <haoqi.shi@xyzrobotics.ai>
2021-08-11 19:01:41 +0800

    add render depth

--- blenderfunc/render/render.py
+++ blenderfunc/render/render.py
@@ -1,6 +1,8 @@
 import os
 import bpy
 import addon_utils
+import imageio
+import numpy as np
 
 
 def render_color(filepath: str = '/tmp/temp.png', samples: int = 32, auto_tile_size: bool = True,
@@ -64,4 +66,55 @@ def render_color(filepath: str = '/tmp/temp.png', samples: int = 32, auto_tile_s
     print('image saved: {}'.format(filepath))
 
 
-__all__ = ['render_color']
+def render_depth(filepath: str = '/tmp/temp.png', depth_scale=0.00005):
+    addon_utils.enable("render_auto_tile_size")
+    scene = bpy.data.scenes['Scene']
+    scene.render.engine = 'CYCLES'
+    scene.cycles.device = 'GPU'
+    scene.cycles.samples = 1
+    scene.cycles.debug_bvh_type = "STATIC_BVH"
+    scene.cycles.debug_use_spatial_splits = True
+    scene.render.use_persistent_data = True
+    scene.ats_settings.is_enable = True
+    bpy.context.view_layer.cycles.use_denoising = False
+    scene.cycles.use_denoising = False
+    scene.render.threads_mode = 'FIXED'
+    scene.render.threads = 1
+    scene.render.use_simplify = True
+    scene.render.simplify_subdivision_render = 3
+    scene.cycles.diffuse_bounces = 0
+    scene.cycles.glossy_bounces = 0
+    scene.cycles.ao_bounces_render = 0
+    scene.cycles.max_bounces = 0
+    scene.cycles.transmission_bounces = 0
+    scene.cycles.transparent_max_bounces = 0
+    scene.cycles.volume_bounces = 0
+
+    output_dir = os.path.dirname(filepath)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+
+    scene.use_nodes = True
+    node_tree = scene.node_tree
+    for node in node_tree.nodes:
+        node_tree.nodes.remove(node)
+    render_layers_node = node_tree.nodes.new('CompositorNodeRLayers')
+    file_output_node = node_tree.nodes.new('CompositorNodeOutputFile')
+    file_output_node.base_path = os.path.dirname(filepath)
+    file_output_node.file_slots['Image'].path = 'image'
+    file_output_node.format.file_format = 'OPEN_EXR'
+    node_tree.links.new(render_layers_node.outputs[2], file_output_node.inputs[0])
+    bpy.context.scene.frame_current = 1
+    bpy.ops.render.render(use_viewport=True)
+
+    output_exr = os.path.join(output_dir, 'image0001.exr')
+    depth = imageio.imread(output_exr)
+    depth = depth[:, :, 0]
+    depth = depth / depth_scale
+    depth = depth.astype(np.uint16)
+    imageio.imwrite(filepath, depth)
+    print('image saved: {}'.format(filepath))
+    os.remove(output_exr)
+
+
+__all__ = ['render_color', 'render_depth']
